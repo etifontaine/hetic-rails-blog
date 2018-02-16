e0aa02ad9412f92294215cc698db909ce53ada1b
EtienneFontaine <etienne.fontaine1@gmail.com>
2018-02-16 15:56:33 +0100

    remove stuffs

--- db/schema.rb
+++ db/schema.rb
@@ -12,13 +12,6 @@
 
 ActiveRecord::Schema.define(version: 20180216093803) do
 
-  create_table "categories", force: :cascade do |t|
-    t.string "name"
-    t.string "branch"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "comments", force: :cascade do |t|
     t.text "content"
     t.integer "post_id"
@@ -32,7 +25,6 @@ ActiveRecord::Schema.define(version: 20180216093803) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
-    t.integer "category_id"
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
