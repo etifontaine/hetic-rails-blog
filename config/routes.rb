25a417a6f22bc2c78d648721bcacf9f1d0e32950
EtienneFontaine <etienne.fontaine1@gmail.com>
2018-02-16 10:05:04 +0100

    add locales

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => {:registrations => "registrations"}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  root to: 'pages#index'
+  scope "(:locale)", :locale => /en|fr/ do
+    root to: 'pages#index'
+    get "pages/index"
+  end
 
   devise_scope :user do
     get 'login', to: 'devise/sessions#new'
